25703008c183fd78f790b1cdb1ed1736e517dc0b
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-04-20 19:26:06 -0300

    implement take a picture

--- AppDemo/AppDemo/AppDemo.Android/MainActivity.cs
+++ AppDemo/AppDemo/AppDemo.Android/MainActivity.cs
@@ -6,18 +6,20 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Media;
 
 namespace AppDemo.Droid
 {
     [Activity(Label = "AppDemo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+            await CrossMedia.Current.Initialize();
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
